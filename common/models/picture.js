4368fde1b644f8d30050abe93dc2cd0ae1fd149a
Paulo McNally <paulomcnally@gmail.com>
2017-10-27 14:47:33 -0600

    set userId from accessToken

--- common/models/picture.js
+++ common/models/picture.js
@@ -2,4 +2,10 @@
 
 module.exports = function(Picture) {
 
+  // set userId
+  Picture.observe('before save', function filterProperties(ctx, next) {
+    ctx.instance.userId = ctx.options.accessToken.userId;
+    next();
+  });
+
 };

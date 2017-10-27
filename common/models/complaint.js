d3ccfb09869637be9ce603613faee4f4e5155952
Paulo McNally <paulomcnally@gmail.com>
2017-10-27 14:48:56 -0600

    set userId from accessToken

--- common/models/complaint.js
+++ common/models/complaint.js
@@ -2,4 +2,10 @@
 
 module.exports = function(Complaint) {
 
+  // set userId
+  Complaint.observe('before save', function filterProperties(ctx, next) {
+    ctx.instance.userId = ctx.options.accessToken.userId;
+    next();
+  });
+
 };
